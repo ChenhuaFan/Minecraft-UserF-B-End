f88fb96de7ba7e8d2c8823abb699faf6dd73a1ef
ChenhuaFan <805901483@qq.com>
2016-10-28 20:40:23 +0800

    form_check

---
+++ wwwroot/CheckFormResg.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 2016/10/28
+ * Time: 14:22
+ */
+include ("inlcudes/CheckFormL.php");
+
+$checkformreg = new CheckFormR($_POST['username'],$_POST['password'],$_POST);
+$checkformreg->checkunity();
+?>
